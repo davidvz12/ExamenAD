3ac6835c82a0122c6712b9b1d442baa31b6f377d
davidvz12 <victor.zapata2016@uteq.edu.ec>
2021-03-12 13:59:23 -0500

    AD

--- src/java/webservices/WS_ValidarUser.java
+++ src/java/webservices/WS_ValidarUser.java
@@ -8,7 +8,7 @@ package webservices;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-
+import conexion.*;
 /**
  *
  * @author david
@@ -17,10 +17,32 @@ import javax.jws.WebParam;
 public class WS_ValidarUser {
 
     /**
-     * This is a sample web service operation
+     * Web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName = "Loguearse")
+    public Usuarios Loguearse(@WebParam(name = "usuario") String usuario, @WebParam(name = "clave") String clave) {
+        //TODO write your implementation code here:
+        Usuarios usu = new Usuarios();
+        Conexion c = new Conexion();
+        usu = c.Loguearse(usuario, clave);
+        return usu;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "RegistrarUser")
+    public Usuarios RegistrarUser(@WebParam(name = "user") String user, @WebParam(name = "clave") String clave, @WebParam(name = "tipo") String tipo, @WebParam(name = "estado") String estado, @WebParam(name = "correo") String correo, @WebParam(name = "nombre") String nombre) {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "RegistrarPedido")
+    public Pedido RegistrarPedido(@WebParam(name = "id_user") int id_user, @WebParam(name = "estado") String estado, @WebParam(name = "id_product") int id_product, @WebParam(name = "cantidad") int cantidad) {
+        //TODO write your implementation code here:
+        return null;
     }
 }
